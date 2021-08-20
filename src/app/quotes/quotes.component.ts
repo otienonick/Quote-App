57b18fde4c34605ddbc204d2cfe334e91bf4e8e0
otienonick <nick.odongi@student.moringaschool.com>
2021-08-20 16:49:41 +0300

    modified image folder

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import{Quote} from '../quote';
+
 
 @Component({
   selector: 'app-quotes',
@@ -6,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  quotes:Quote[] = [
+    new Quote('Jackson','Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quos doloribus molestiae neque veniam.','john'),
+    new Quote('Jackson','Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quos doloribus molestiae neque veniam.','john'),
+    new Quote('Jackson','Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quos doloribus molestiae neque veniam.','john'),
+    new Quote('Jackson','Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quos doloribus molestiae neque veniam.','john'),
+    new Quote('Jackson','Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quos doloribus molestiae neque veniam.','john'),
+    new Quote('Jackson','Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quos doloribus molestiae neque veniam.','john'),
 
+  ];
+  //   toggleDetails(index: number){
+  //       this.quotes[index].quoteDetails = !this.quotes[index].quoteDetails;
+  //     }
+    
   constructor() { }
 
   ngOnInit(): void {
