2400ea14e4d122c8203d88dba532322a24d81e87
otienonick <nick.odongi@student.moringaschool.com>
2021-08-20 16:49:04 +0300

    added quote-details component

---
+++ src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,18 @@
+import { Quote } from '../quote';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+  @Input()
+  quote!: Quote;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
