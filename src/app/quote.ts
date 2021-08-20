dc4a7858019e6619cb599afc91b097825ff7e457
otienonick <nick.odongi@student.moringaschool.com>
2021-08-20 16:47:14 +0300

    added quotes section

---
+++ src/app/quote.ts
@@ -0,0 +1,12 @@
+export class Quote {
+    author:string;
+    quote:string;
+    quoteDetails:string;
+
+    constructor(author:string,quote:string,quoteDetails:string)
+    {
+this.author = author;
+this.quote = quote;
+this.quoteDetails = quoteDetails;
+}
+}
